4cdfcd9d29195a728222997c87835ce29c7afe80
majewskikar <majewskikar@gmail.com>
2023-08-03 16:09:44 +0200

    PRACA

--- szukanie_liter.py
+++ szukanie_liter.py
@@ -1,35 +1,67 @@
 import cv2 as cv
 import numpy as np
 import glob
-import pyautogui as pg
+import os
+from PIL import ImageGrab
+from time import time
 
 
-images = [cv.imread(file, cv.IMREAD_GRAYSCALE) for file in (glob.glob("litery/*.jpg"))]
+# #wyswietlanie obrazu
+# loop_time = time()
+# while(True):
+#     screenshot = ImageGrab.grab(bbox =(0, 0, 1500, 1500))
+#     screenshot = np.array(screenshot)
+#     screenshot = cv.cvtColor(screenshot, cv.COLOR_RGB2BGR)
+#
+#     cv.imshow('computer_vision', screenshot)
+#
+#     # get an updated image of the game
+#
+#     # debug the loop rate
+#     print('FPS {}'.format(1 / (time() - loop_time)))
+#     loop_time = time()
+#
+#     # press 'q' with the output window focused to exit.
+#     # waits 1 ms every loop to process key presses
+#     if cv.waitKey(1) == ord('q'):
+#         cv.destroyAllWindows()
+#         break
+
+
+
+#szukanie liter
+images = [cv.imread(file, cv.IMREAD_UNCHANGED) for file in (glob.glob("litery/*.jpg"))]
+dir_names = os.listdir('litery')
+
+assert images is not None, "file could not be read, check with os.path.exists()"
+
+
+
 #testowa plansza
-test = cv.imread('test.jpg', cv.IMREAD_UNCHANGED)
-test1 = cv.cvtColor(test, cv.COLOR_BGR2GRAY)
+test1 = cv.imread('test.jpg', cv.IMREAD_UNCHANGED)
+#test1 = cv.cvtColor(test, cv.COLOR_BGR2GRAY)
 for litery in images:
-    result =  pg.locateAllOnScreen(litery, confidence =0.5)
+    letter_name = dir_names[0][0]
     result = cv.matchTemplate(litery, test1, cv.TM_CCOEFF_NORMED)
     min_val, max_val, min_loc, max_loc = cv.minMaxLoc(result)
-
     width = litery.shape[1]
     height = litery.shape[0]
-    ## #cv2.rectangle(test, max_loc, (max_loc[0] + width, max_loc[1] + height), (0,255,255), 2)
 
-    threshold = .90
+    threshold = 0.85
     yloc, xloc = np.where(result>= threshold)
 
     rectangles = []
+    rectangles_size = []
     for (x,y) in zip(xloc, yloc):
         rectangles.append([int(x), int(y), int(width), int(height)])
         rectangles.append([int(x), int(y), int(width), int(height)])
-    print(len(rectangles))
-    rectangles, weights = cv.groupRectangles(rectangles, 1, 0.2)
+    rectangles, weights = cv.groupRectangles(rectangles, 1, 0.5)
 
     for (x,y,w,h) in rectangles:
-        cv.rectangle(test, (x,y), (x+w, y+h),(0,255,255), 2)
-
-cv.imshow('test', test)
+        cv.rectangle(test1, (x,y), (x+w, y+h),(0,255,255), 2)
+        rectangles_size.append([x , y, w+x ,h+y])
+    dir_names.pop(0)
+    #print(rectangles_size)
+cv.imshow('test', test1)
 cv.waitKey()
 cv.destroyAllWindows()
