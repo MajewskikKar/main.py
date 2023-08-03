45493a51734660e46076ba8c4ed15b6b9ee061a0
majewskikar <majewskikar@gmail.com>
2023-08-03 16:10:18 +0200

    DODANIE PLIKÓW

--- testowanie_rozwiazan.py
+++ testowanie_rozwiazan.py
@@ -1,44 +1,13 @@
-import cv2
 import numpy as np
-import pyautogui as pg
+import sys
+import cv2 as cv
+import os
 
-# take a screenshot to store locally
-#screenshot = pg.screenshot('screenshot.png')
+template = cv.imread('test.jpg', cv.IMREAD_UNCHANGED)
+litera = cv.imread('litery/a.jpg', cv.IMREAD_UNCHANGED)
 
-# take a screenshot to locate objects on
-screenshot = pg.screenshot()
-
-# adjust colors
-screenshot = cv2.cvtColor(np.array(screenshot), cv2.COLOR_RGB2BGR)
-
-# locate a single object in a screenshot
-#board = pg.locateOnScreen('board.png')
-
-# draw rectangle around the object
-#cv2.rectangle(
-#    screenshot,
-#    (board.left, board.top),
-#    (board.left + board.width, board.top + board.height),
-#    (0, 255, 255),
-#    2
-#)
-
-# detect several objects on screenshot
-for pawn in pg.locateAllOnScreen('a.jpg', confidence=0.7):
-    # draw rectangle around the object
-    cv2.rectangle(
-        screenshot,
-        (pawn.left, pawn.top),
-        (pawn.left + pawn.width, pawn.top + pawn.height),
-        (0, 0, 255),
-        2
-    )
-
-# display screenshot in a window
-cv2.imshow('Screenshot', screenshot)
-
-# escape condition
-cv2.waitKey(0)
-
-# clean up windows
-cv2.destroyAllWindows()
+result = cv.matchTemplate(litera, template, cv.TM_CCOEFF_NORMED)
+thershold = 0.95
+location = np.where(result >= thershold)
+location = list(zip(*location[::-1]))
+print(location)
